04f0af03964f98c98549a86ff3ea90784fb21859
jkcryptolock <jkconno@gmail.com>
2019-07-15 16:11:34 -0500

    Attempted Parse

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,20 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  if (json === 'true') {
+    return true;
+  }
+  if (json === 'false') {
+    return false;
+  }
+  if (json === 'null') {
+    return null;
+  }
+  if (json.length === json.replace(/^0-9/gi, '').length) {
+    return Number(json);
+  }
+  if ((json.startsWith('"') || json.startsWith("'")) && (json.endsWith('"') || json.endsWith("'"))) {
+    return json.slice(1, -1);
+  }
+  
 };
