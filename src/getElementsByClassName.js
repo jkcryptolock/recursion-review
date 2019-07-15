05202cf0b0dd45950a399bb2412e4e736f11d828
jkcryptolock <jkconno@gmail.com>
2019-07-15 15:42:25 -0500

    Add support for Stringify and GetElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  const Node = document.body;
+  let result = [];
+  
+  let recursive = function(elem) {
+    let children = elem.childNodes; 
+    if(elem.classList && elem.classList.contains(className)) {
+      result.push(elem); 
+    }
+    if(elem.childNodes) {
+      for(let i = 0; i < children.length; i++) {
+        recursive(children[i]);
+        }
+    }
+  };
+
+  recursive(Node);
+
+  return result;
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,35 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //if it's boolean, number, or string - return value in quotes
+  //if it's an Array and containes a function, or undefined, return null
+  //otherwise skip the value
+  
+  let result = [];
+  
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return '' + obj;
+  }
+  
+  if (typeof obj === 'function' || obj === undefined) {
+    return  '';
+  }
+  
+  if (Array.isArray(obj)) {
+    return '[' + obj.map(ele => stringifyJSON(ele)).join(',') + ']';
+  }
+  
+  if (typeof obj === 'object') {
+    for (var key in obj) {
+      if (typeof obj[key] !== 'function' && obj[key] !== undefined) {
+        result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]))
+      }
+    }
+    return '{' + result + '}';
+  }
+  
 };
